4cc670649a64222f257f2461c3482c67d10faf2d
Jesse Both <jesse9both@gmail.com>
2023-03-09 23:42:55 -0500

    Working

--- js/code.js
+++ js/code.js
@@ -179,176 +179,20 @@ function cal_icon() {
     "_2x.png";
   }
 }
-color = "#0f99e3";
-function ub_color() {
-  color = "#015bbc";
-}
+color = "#7300ff";
+
 function start_color() {
   /*date/time*/
   document.getElementById("time").style.color = color;
   document.getElementById("month").style.color = color;
 }
 
-profiles = ["../images/profile.png", "../images/ub.png", "../images/anon.png"];
+profiles = ["../images/anon.png"];
 profile_info = {
-  "../images/profile.png":
-    "<img id='img_main' src='../images/profile.png' width='36' height='36'/>",
-  "../images/ub.png":
-    "<img id='img_user1' src='../images/ub.png' width='36' height='36'/>",
   "../images/anon.png":
     "<img id='img_user2' src='../images/anon.png' width='36' height='36'/>",
 };
-function main_tab() {
-  document.getElementById("google").classList.remove("virus");
-  color = "#0f99e3";
-  img = "../images/google.png";
-  height = "100px";
-  width = "300px";
-  email = "../images/gmail.png";
-  email_link = "https://mail.google.com/mail/u/0/#inbox";
-  drive_link = "https://drive.google.com/drive/u/1/"
-  cal_link = "https://calendar.google.com/calendar/u/1/r/week"
-  icon_drawer = "../images/app_drawer.png";
-  profile = "../images/profile.png";
-  _top = "-470px";
-  _left = "0px";
-  app_top = "-86px";
-  mail_top = "-44px";
-  l = "0";
-  all_day = "-470px";
-  all_day_left = "20px";
-  drawthing(false);
-  set_tab(color, img, height, width, profile, email, email_link, drive_link, cal_link, icon_drawer, all_day);
-  document.getElementById("img_user1").addEventListener("click", first_tab);
-  document.getElementById("img_user2").addEventListener("click", second_tab);
-  document.getElementById("apps").addEventListener("click", appToggle);
-  document.getElementById("custom_cal").addEventListener("click", calredirect);
-  document.querySelectorAll('.app').forEach(item => {
-      item.addEventListener('click', event => {
-        app_clicked_on()
-      })
-    })
-    flip_apps("app_select_1", "app_select_2")
-    history.replaceState(null,'');
-}
 
-function first_tab() {
-  document.getElementById("google").classList.remove("virus");
-  color = "#015bbc";
-  img = "../images/UB_logo.png";
-  height = "120px";
-  width = "250px";
-  email = "../images/gmail_1.png";
-  email_link = "https://mail.google.com/mail/u/1/#inbox";
-  drive_link = "https://drive.google.com/drive/u/1/"
-  cal_link = "https://calendar.google.com/calendar/u/1/r/week"
-  icon_drawer = "../images/app_drawer_UB.png";
-  profile = "../images/ub.png";
-  _top = "-490px";
-  _left = "0px";
-  app_top = "-86px";
-  mail_top = "-44px";
-  l = "0";
-  all_day = "-490px";
-  all_day_left = "20px";
-  drawthing(false);
-  set_tab(color, img, height, width, profile, email, email_link, drive_link, cal_link, icon_drawer, all_day); 
-  document.getElementById("img_main").addEventListener("click", main_tab);
-  document.getElementById("img_user2").addEventListener("click", second_tab);
-  document.getElementById("apps").addEventListener("click", appToggle);
-  document.querySelectorAll('.app').forEach(item => {
-      item.addEventListener('click', event => {
-        app_clicked_on()
-      })
-    })
-  flip_apps("app_select_2", "app_select_1")
-  history.replaceState({"user": "ub"},'');
-}
-
-function second_tab() {
-  document.getElementById("google").classList.add("virus");
-  color = "#393939";
-  img = "../images/glitch.png";
-  height = "175px";
-  width = "137px";
-  email = "../images/gmail_2.png";
-  email_link = "https://mail.google.com/mail/u/2/#inbox";
-  drive_link = "https://drive.google.com/drive/u/2/"
-  cal_link = "https://calendar.google.com/calendar/u/2/r/week"
-  icon_drawer = "../images/app_drawer_glitch.png";
-  profile = "../images/anon.png";
-  _top = "-545px";
-  _left = "-8px";
-  app_top = "-62px";
-  mail_top = "-32px";
-  l = "0px";
-  all_day = "-545px";
-  a_d_left = "20px";
-  set_tab(color, img, height, width, profile, email, email_link, drive_link, cal_link, icon_drawer, all_day);
-  document.getElementById("img_main").addEventListener("click", main_tab);
-  document.getElementById("img_user1").addEventListener("click", first_tab);
-      document.getElementById("apps").addEventListener("click", appToggle);
-    document.querySelectorAll('.app').forEach(item => {
-        item.addEventListener('click', event => {
-          app_clicked_on()
-        })
-      })
-
-
-  document.getElementById("img_google").style.position = "unset";
-  drawthing(true);
-  flip_apps("app_select_2", "app_select_1")
-  history.replaceState({"user": "anon"},'');
-}
-
-function set_tab(
-  color, img, height, width, profile, email, email_link, drive_link, cal_link, icon_drawer, all_day) {
-  dim = " id='img_google' style=height:" + height + ";width:" + width;
-  document.getElementById("google").innerHTML =
-    "<img src=" + img + dim + "></img>";
-  document.getElementById("google").style.height = height;
-  document.getElementById("google").style.width = width;
-  document.getElementById("google").style.marginLeft = "auto";
-  document.getElementById("google").style.marginRight = "auto";
-
-  document.getElementsByClassName("searchTerm")[0].style.borderColor = color;
-  document.getElementsByClassName("searchTerm")[0].style.color = color;
-  document.getElementsByClassName("searchButton")[0].style.background = color;
-  document.getElementsByClassName("searchButton")[0].style.borderColor = color;
-  swap(profile);
-  document.getElementById("pic").src = profile;
-  document.getElementById("user1").innerHTML = profile_info[profiles[1]];
-  document.getElementById("user2").innerHTML = profile_info[profiles[2]];
-  document.getElementById("apps").src = icon_drawer;
-  document.getElementById("email").src = email;
-  document.getElementById("email_link").setAttribute("href", email_link);
-  document.getElementById("drive_link").setAttribute("href", drive_link);
-  document.getElementById("cal_link").setAttribute("href", cal_link);
-  cal_icon();
-  document.getElementById("time").style.color = color;
-  document.getElementById("month").style.color = color;
-  // document.getElementById("appDrawer").style.top = app_top;
-  // document.getElementById("gmail").style.top = mail_top;
-  // document.getElementById("l").style.marginLeft = l; 
-  document.getElementById("all_day").style.marginTop = all_day;
-  // document.getElementById("all_day").style.marginLeft = all_day_left;
-}
-
-function swap(profile) {
-  i = 0;
-  while (profiles[i] != profile) {
-    i++;
-  }
-  if (i == 1) {
-    cur = profiles[0];
-    profiles[0] = profiles[i];
-    profiles[i] = cur;
-  } else if (i == 2) {
-    cur = profiles[0];
-    next_ = profiles[1];
-    profiles = [profiles[i], cur, next_];
-  }
-}
 var interval_set = false;
 function drawthing(check) {
   if (check) {
@@ -385,7 +229,7 @@ function drawthing(check) {
       // ctx.fillStyle = "rgba(0,0,0,.06)";
       ctx.fillRect(0, 0, c.width, c.height);
 
-      ctx.fillStyle = "#3366ff"; //blue text
+      ctx.fillStyle = color; 
       ctx.font = font_size + "px arial";
       //looping over drops
       for (var i = 0; i < drops.length; i++) {
@@ -427,23 +271,4 @@ function drawthing(check) {
       c.style.visibility = "hidden";
     }
   }
-}
-
-function get_user(){
-  if(history.state != null){
-    if(history.state["user"] == "ub"){
-      first_tab()
-    }
-    else if(history.state["user"] == "anon"){
-      second_tab()
-    }
-  }
-}
-
-function flip_apps(flip, flop){
-  undo_appToggle()
-  document.getElementById(flop).classList.add("inactive");
-  document.getElementById(flop).classList.remove("active");
-  document.getElementById(flip).classList.add("active");
-  document.getElementById(flip).classList.remove("inactive");
 }
--- js/init_agenda.js
+++ js/init_agenda.js
@@ -1,18 +1,11 @@
 window.onload = run
 function run() {
-    document.getElementById("calendar").addEventListener("click", toggleCal);
-    document.getElementById("all_day").addEventListener("click", rotate_click);
-    document.querySelector('#description_container').addEventListener('click', description_clicked_on);
-
-    document.getElementById("red_dot").addEventListener("click", hide_description);
+    rand_image = Math.floor(Math.random() * 60)+1
     document.querySelector('.searchTerm').addEventListener('click', clickyClick);
     document.querySelector('.searchButton').addEventListener('click', search);
     document.getElementById("pic").addEventListener("click", userToggle);
-    document.getElementById("img_user1").addEventListener("click", first_tab);
-    document.getElementById("img_user2").addEventListener("click", second_tab);
 
     document.getElementById("apps").addEventListener("click", appToggle);
-    document.getElementById("custom_cal").addEventListener("click", calredirect);
     document.querySelectorAll('.app').forEach(item => {
         item.addEventListener('click', event => {
           app_clicked_on()
@@ -20,16 +13,11 @@ function run() {
       })
     document.querySelector('.searchTerm').addEventListener('keypress', clickyClick)
 
-    get_user()
     updateTime();
-    set_all_day();
     wind()
     cal_icon()
     start_color()
-    listeners()
+    document.getElementById("img_google").src = "../images/"+String(rand_image)+".png"
+    drawthing(true)
     setInterval(updateTime, 100);
-}
-
-function calredirect(){
-  chrome.tabs.update({active: false, url: "file:///C:/Users/jesse/IdeaProjects/Personal-Projects/New_Tab/src/main/calendar/calendar.html"});
 }
